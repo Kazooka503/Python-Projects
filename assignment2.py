55c8a062c7a5ba87db737f5d2c8be607ff4a17cb
Austin Krzciok <38452989+Kazooka503@users.noreply.github.com>
2020-06-16 17:19:17 -0700

    Assignment2 Update
    Corrects problem with assignment 2 by adding instantiated object examples of each class with defined attributes

--- assignment2.py
+++ assignment2.py
@@ -1,14 +1,52 @@
 class Employee:
-    name = 'No Name Provided'
-    email = ' '
-    password = '123abcd'
-    id_number = 0
+    def __init__(self, first_name, last_name, email,
+                 password, id_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.id_number = id_number
+
+    def checkout(self):
+        print("Calculating your total, please wait...")
+
+    def promo(self):
+        print("Calculating your rewards, please wait...")
 
 class Clerk(Employee):
-    pay = 15.00
-    security_access = 'Low'
+
+    def employee_discount(self):
+        print("Calculating your discount, please wait...")
+
+    def clock_in(self):
+        print("Timecard Punched, get to work!")
 
 class Manager(Employee):
-    pay = 22.00
-    security_access = 'High'
-    
+
+    def refund(self):
+        print("Calculating your refund, sorry for our mistake...")
+
+    def supply_order(self):
+        print("Order sent to commissary successful")
+
+terry = Clerk("Terry", "Evans", "tbear@gmail.com", "SilverSpark123", "1")
+print(terry.first_name + " " + terry.last_name)
+print("Email: " + terry.email)
+print("Password: " + terry.password)
+print("ID#: " + terry.id_number)
+terry.checkout()
+terry.promo()
+terry.employee_discount()
+terry.clock_in()
+
+print(" ")
+
+casey = Manager("Casey", "Smith", "bdl@yahoo.com", "vintageoak", "2")
+print(casey.first_name + " " + casey.last_name)
+print("Email: " + casey.email)
+print("Password: " + casey.password)
+print("ID#: " + casey.id_number)
+casey.checkout()
+casey.promo()
+casey.refund()
+casey.supply_order()
