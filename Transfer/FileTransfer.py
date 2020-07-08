1997929d0d10a954b5c5abffc57a85a6ec1abb10
Kaz <austinkrz@gmail.com>
2020-07-07 18:12:25 -0700

    File Transfer Program Fix
    Implements code that checks for files in source directory that have been modified in the last 24hrs and moves them to destination folder - uses get() function to retreive origin and destination paths and formats them for use in glob by splitting drive off of directory name

--- Transfer/FileTransfer.py
+++ Transfer/FileTransfer.py
@@ -1,22 +1,48 @@
 from tkinter import *
 from tkinter import filedialog
+from pathlib import Path
 import glob
 import os
 import shutil
 import datetime
 import sys
-import transfer
+
+def FileListCapture(path, type):
+    #returns a list of filenames that match path
+    return glob.glob(path + "*" + type)
 
 def browse_a():
-    originPath = filedialog.askdirectory()
-    e1.insert(0, originPath)
+    x = os.path.splitdrive(filedialog.askdirectory())
+    y = x[1]
+    e1.insert(0, y)
 
 def browse_b():
-    destinationPath = filedialog.askdirectory()
-    e2.insert(0, destinationPath)
+    x = os.path.splitdrive(filedialog.askdirectory())
+    y = x[1]
+    e2.insert(0, y)
 
+def check():
+    originPath = e1.get() + "/"
+    destinationPath = e2.get() + "/"
+    fileType = ".txt"
+    source = FileListCapture(originPath, fileType)
+    print(originPath)
+    print(destinationPath)
+    print(source)
+    for file in source:
+        #Get last modified date and current date
+        modDate = datetime.datetime.fromtimestamp(os.path.getmtime(file))
+        currentDate = datetime.datetime.today()
+
+        filePathList = file.split("\\") # create list from filepath
+        filename = filePathList[-1]
+
+        modifyDateLimit = modDate + datetime.timedelta(days=1)
+        if modifyDateLimit > currentDate:
+            shutil.copy2(file, destinationPath + filename)
+    
 root = Tk()
-root.title("Sample GUI")
+root.title("File Transfer")
 
 frame = LabelFrame(root, padx=5, pady=5)
 frame.pack(padx=0, pady=0)
@@ -33,8 +59,8 @@ e2.grid(row=2, column=1, columnspan=5, padx=20)
 spacer = Label(frame, text = " ")
 browse1 = Button(frame, text="Browse...", padx=20, command=browse_a)
 browse2 = Button(frame, text="Browse...", padx=20, command=browse_b)
-check = Button(frame, text="Check for files...", pady=10, command=transfer)
-close = Button(frame, text="Close Program", padx=5, pady=10, command=exit)
+check = Button(frame, text="Check for files...", pady=10, command=check)
+close = Button(frame, text="Close", padx=20, pady=10, command=exit)
 
 #Painting widget onto the screen
 spacer.grid(row=0, column=0, columnspan=4, pady=5)
@@ -43,6 +69,6 @@ browse2.grid(row=2, column=0, pady=5)
 check.grid(row=3, column=0)
 close.grid(row=3, column=5, padx=5)
 
-root.mainloop()
+
 
 
